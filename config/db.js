632335c041b7fb185fe6bfb43f08e55cf3d1a15a
Hemonto839 <arkaroy839@gmail.com>
2026-05-17 00:23:20 +0600

    hosting the database

--- config/db.js
+++ config/db.js
@@ -8,14 +8,19 @@ const { Pool } = require("pg");
 
 // Create a new PostgreSQL connection pool.
 // This tells Node.js how to connect to your PostgreSQL database.
-const pool = new Pool({
-    user: process.env.DB_USER, // PostgreSQL username. Default superuser is usually "postgres".
-    host: process.env.DB_HOST, // Since PostgreSQL is installed on your own computer, host is localhost.
-    database: process.env.DB_NAME, // This is the database name you created in pgAdmin.
-    password: process.env.DB_PASSWORD, // This is your PostgreSQL password.
-    port: Number(process.env.DB_PORT),  // Default PostgreSQL port.
-});
+// const pool = new Pool({
+//     user: process.env.DB_USER, // PostgreSQL username. Default superuser is usually "postgres".
+//     host: process.env.DB_HOST, // Since PostgreSQL is installed on your own computer, host is localhost.
+//     database: process.env.DB_NAME, // This is the database name you created in pgAdmin.
+//     password: process.env.DB_PASSWORD, // This is your PostgreSQL password.
+//     port: Number(process.env.DB_PORT),  // Default PostgreSQL port.
+// });
 
+const pool = new Pool ({
+    connectionString: process.env.DATABASE_URL,
+    ssl: 
+        process.env.NODE_ENV === "production" ? { rejectUnauthorized: false} : false,
+})
 
 // Export the pool so other files, like server.js, can use it.
 module.exports = pool;
